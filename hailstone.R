fd62525ddaff94afee7ae7ac67adb567b0b8cc2a
priscillafrema <70357745+priscillafrema@users.noreply.github.com>
2020-11-08 17:16:11 -0800

    Add files via upload

---
+++ hailstone.R
@@ -0,0 +1,16 @@
+seqlen = c()
+for (i in 1:1000) {
+  n=i
+  counter=1
+  while (n!= 1){
+    if (n%%2==0) {
+      n<-as.integer(n/2)
+    }
+    else {
+      n<-as.integer(3*n+1)
+    }
+    counter = counter + 1
+  }
+  seqlen= c(seqlen, counter)
+}
+plot(seqlen)
